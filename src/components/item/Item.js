ce22505c86ab790416cce8d53bb556844d5f8c4a
Watife <watife14@yahoo.com>
2018-07-20 01:04:54 +0100

    map corrected

--- src/components/item/Item.js
+++ src/components/item/Item.js
@@ -5,7 +5,7 @@ class Item extends Component {
      
     render() {
        const IncItem = this.props.Income.map((value) => {
-        //    if (value.Type === 'inc' && value.Description !== '' && value.Value !== '') {
+           if (value.Type === 'inc' && value.Description !== '' && value.Value !== '') {
                 return (
                     <div className="income__list" key={value.Id}>
                         <div className="item clearfix" id="income-0"><div className="item__description">{value.Description}</div>
@@ -17,26 +17,29 @@ class Item extends Component {
                             </div>
                         </div>
                     </div>    
-            )})
-    //    })
+            )} else {
+                return value;
+            }
+       })
         const ExpItem = this.props.Expense.map((value) => {
-                if (value.Type === 'exp' && value.Description !== '' && value.Value !== '') 
-                    return (
-                        <div className="expenses__list" key={value.Id}>
-                            <div className="item clearfix" id="expense-0">
-                                <div className="item__description">{value.Description}</div>
-                                <div className="right clearfix">
-                                    <div className="item__value" >- {value.Value}</div>
-                                    <div className="item__percentage">21%</div>
-                                    <div className="item__delete">
-                                        <button className="item__delete--btn"><i className="ion-ios-close-outline"></i></button>
-                                    </div>
+            if (value.Type === 'exp' && value.Description !== '' && value.Value !== '') {
+                return (
+                    <div className="expenses__list" key={value.Id}>
+                        <div className="item clearfix" id="expense-0">
+                            <div className="item__description">{value.Description}</div>
+                            <div className="right clearfix">
+                                <div className="item__value" >- {value.Value}</div>
+                                <div className="item__percentage">21%</div>
+                                <div className="item__delete">
+                                    <button className="item__delete--btn"><i className="ion-ios-close-outline"></i></button>
                                 </div>
-                            </div>        
-                        </div>
-                    )})
-            // return value; //this is due to map requiring a return 
-        // })
+                            </div>
+                        </div>        
+                    </div>
+                )} else {
+                return value;
+                }
+        })
 
         return (
             <div className="bottom">
